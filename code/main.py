b976719b5db03f2f9e88cc3a005d39665adbd067
IVEN-CN <13377529851@stu.gdou.edu.cn>
2023-12-02 20:43:50 +0800

    添加程序入口

--- code/main.py
+++ code/main.py
@@ -23,24 +23,25 @@ def detectCOLOR(_img, lowrange, uprange):
     return mask
 
 
-# 创建摄像头对象
-cap = cv2.VideoCapture(0)
-# 创建串口对象
-ser = serial.Serial()  # 需要完善串口名和波特率
+if __name__ == '__main__':
+    # 创建摄像头对象
+    cap = cv2.VideoCapture(0)
+    # 创建串口对象
+    ser = serial.Serial()  # 需要完善串口名和波特率
 
-while cap.isOpened():
-    sign = 0
-    ret, frame = cap.read()
-    img_Gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 用于识别二维码的灰度图
-    img_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # 用于颜色识别的HSV色彩空间
-    # 读取个字节的串口数据
-    sign_QR = ser.read(1)
+    while cap.isOpened():
+        sign = 0
+        ret, frame = cap.read()
+        img_Gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 用于识别二维码的灰度图
+        img_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # 用于颜色识别的HSV色彩空间
+        # 读取个字节的串口数据
+        sign_QR = ser.read(1)
 
-    if sign_QR == 1:
-        """识别到黑线"""
-        # 尝试识别二维码
-        data_QR, sign = detectQR(img_Gray)
-        # 传回信号
-        ser.write(sign)
-    else:
-        pass
+        if sign_QR == 1:
+            """识别到黑线"""
+            # 尝试识别二维码
+            data_QR, sign = detectQR(img_Gray)
+            # 传回信号
+            ser.write(sign)
+        else:
+            pass

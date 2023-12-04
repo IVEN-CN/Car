9f30150d90531d80ab6194816039870c864364ff
IVEN-CN <13377529851@stu.gdou.edu.cn>
2023-12-04 10:36:56 +0800

    优化了识别二维码和颜色识别的函数，使两个过程分开进行，当识别到二维码的时候传回信号1，然后进行颜色识别，识别到正确的颜色的时候传回信号1，前提是有一个合适大小的卷积核，如果卷积核在二值化后的图片内即识别颜色成功

--- code/main.py
+++ code/main.py
@@ -1,47 +1,62 @@
 """执行过程的主文件
 1.根据树莓派的串口设置串口名称和波特率
-2.单片机没有传回的信号时是不是0？
 3.创建一个合适大小的卷积核"""
 
 import cv2
 import serial
 import numpy as np
 
+check_arr = np.ones()  # 需要输入卷积核尺寸
 
-def detectQR(_img) -> tuple:
+
+def detectQR() -> tuple:
     """识别二维码"""
-    det = cv2.QRCodeDetector()
-    try:
-        codeinfo, points, straight_qrcode = det.detectAndDecode(_img)
-        return codeinfo, 1
-    except:
-        return None, 0
+    # 创建摄像头对象
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        ret, frm = cap.read()
+        if ret:
+            _img = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+            det = cv2.QRCodeDetector()
+            codeinfo, points, straight_qrcode = det.detectAndDecode(_img)
+            if codeinfo != '':
+                return codeinfo, 1
 
 
-def detectCOLOR(_img, lowrange, uprange):
-    mask = cv2.inRange(_img, lowrange, uprange)
-    return mask
+def detectCOLOR(lowrange, uprange):
+    """颜色识别函数"""
+    # 创建摄像头对象
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        ret, frm = cap.read()
+        if ret:
+            _img = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+            mask = cv2.inRange(_img, lowrange, uprange)
+            if np.isin(check_arr, mask):
+                return mask, 1
+
+
+def readfile(sign):
+    if sign == '11':
+        filename = 'Red'
+    if sign == '22':
+        filename = 'Green'
+    if sign == '33':
+        filename = 'Blue'
+
+    arr = np.load(filename)
+    return arr
 
 
 if __name__ == '__main__':
-    # 创建摄像头对象
-    cap = cv2.VideoCapture(0)
     # 创建串口对象
-    ser = serial.Serial()  # 需要完善串口名和波特率
+    ser = serial.Serial()  # 需要完善串口名和波特率(9600)
 
-    while cap.isOpened():
-        sign = 0
-        ret, frame = cap.read()
-        img_Gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 用于识别二维码的灰度图
-        img_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # 用于颜色识别的HSV色彩空间
-        # 读取个字节的串口数据
-        sign_QR = ser.read(1)
+    info, sign = detectQR()
+    # 发送串口信号
+    ser.write(sign)
 
-        if sign_QR == 1:
-            """识别到黑线"""
-            # 尝试识别二维码
-            data_QR, sign = detectQR(img_Gray)
-            # 传回信号
-            ser.write(sign)
-        else:
-            pass
+    # 颜色识别
+    threshold = readfile(info)
+    img,sing_dump = detectCOLOR(threshold[0],threshold[1])
+    # 发送串口信号

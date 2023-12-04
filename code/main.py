baa5947664911ef9469170c145dcdffaa23d06f1
IVEN-CN <13377529851@stu.gdou.edu.cn>
2023-12-04 12:08:21 +0800

    将摄像头对象移至函数外

--- code/main.py
+++ code/main.py
@@ -9,12 +9,10 @@ import numpy as np
 check_arr = np.ones()  # 需要输入卷积核尺寸
 
 
-def detectQR() -> tuple:
+def detectQR(cap_) -> tuple:
     """识别二维码"""
-    # 创建摄像头对象
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-        ret, frm = cap.read()
+    while cap_.isOpened():
+        ret, frm = cap_.read()
         if ret:
             _img = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
             det = cv2.QRCodeDetector()
@@ -23,12 +21,12 @@ def detectQR() -> tuple:
                 return codeinfo, 1
 
 
-def detectCOLOR(lowrange, uprange):
+def detectCOLOR(cap_, lowrange, uprange) -> tuple:
     """颜色识别函数"""
     # 创建摄像头对象
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-        ret, frm = cap.read()
+
+    while cap_.isOpened():
+        ret, frm = cap_.read()
         if ret:
             _img = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
             mask = cv2.inRange(_img, lowrange, uprange)
@@ -36,7 +34,8 @@ def detectCOLOR(lowrange, uprange):
                 return mask, 2
 
 
-def readfile(sign):
+def readfile(sign) -> np.ndarray:
+    """通过识别的二维码选择对应的颜色"""
     if sign == '11':
         filename = 'Red'
     if sign == '22':
@@ -49,15 +48,18 @@ def readfile(sign):
 
 
 if __name__ == '__main__':
+    # 创建摄像头对象
+    cap = cv2.VideoCapture(0)
+
     # 创建串口对象
     ser = serial.Serial()  # 需要完善串口名和波特率(9600)
 
-    info, sign = detectQR()
+    info, sign = detectQR(cap)
     # 发送串口信号：1
     ser.write(sign)
 
     # 颜色识别
     threshold = readfile(info)
-    img,sing_dump = detectCOLOR(threshold[0],threshold[1])
+    img, sing_dump = detectCOLOR(cap, threshold[0], threshold[1])
     # 发送串口信号：2
     ser.write(sing_dump)

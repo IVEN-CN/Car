7b197e15c5a556feabe133414f050563352e4bcc
IVEN-CN <13377529851@stu.gdou.edu.cn>
2023-12-23 15:35:43 +0800

    添加了闪灯指示

--- code/serial_test.py
+++ code/serial_test.py
@@ -1,7 +1,15 @@
 import serial
 import time
+import RPi.GPIO as IO
+
+ser = serial.Serial('/dev/ttyAMA0', 9600)
+IO.setmode(IO.BCM)
+IO.setup(18, IO.OUT)
 
 while 1:
-    ser = serial.Serial('/dev/ttyAMA0', 9600)
+    IO.output(18, IO.HIGH)
     ser.write(b'1')
+    time.sleep(0.25)
+    IO.output(18, IO.LOW)
+    time.sleep(0.25)
 
